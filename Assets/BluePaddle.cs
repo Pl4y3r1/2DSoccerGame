946fa761562fa9ae060b414ebcdc510195589f3c
Pl4y3r1 <chris.g.barry@outlook.com>
2022-12-09 20:57:39 -0700

    Committing everything we've done so far

---
+++ Assets/BluePaddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluePaddle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W)){
+		transform.Translate(0,0.5f,0);
+	} else if(Input.GetKeyDown(KeyCode.S)){
+		transform.Translate(0,-0.5f,0);
+	} else if(Input.GetKeyDown(KeyCode.A)){
+		transform.Translate(-0.5f,0,0);
+	} else if(Input.GetKeyDown(KeyCode.D)){
+		transform.Translate(0.5f,0,0);
+	}
+    }
+}
---
+++ Assets/Circle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+	public bool collided;
+	Vector2 vector;
+	Vector2 vector2;
+
+	void OnCollisionEnter2D(){
+		collided = true;
+		if(vector == Vector2.right){
+			vector = Vector2.left;
+		} else {
+			vector = Vector2.right;
+		}
+	}
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	vector = Vector2.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	OnCollisionEnter2D();
+
+	if(collided){
+		if(vector == Vector2.right){
+			vector = Vector2.left;
+			collided = false;
+		} else {
+			vector = Vector2.right;
+			collided = false;
+		}
+
+	}
+        transform.Translate(vector * 4 * Time.deltaTime);
+
+    }
+}
